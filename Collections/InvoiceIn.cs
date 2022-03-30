73650e1b0e44f8a13f316236b5cacd55d688f76d
Jakub Wandelt 234772 <234772@edu.p.lodz.pl>
2022-03-30 23:07:12 +0200

    InvoiceIn()

---
+++ Collections/InvoiceIn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    internal class InvoiceIn
+    {
+        IUser delieveredBy;
+        IUser receivedBy;
+        private string invoiceNumber;
+        private int day;
+        private int month;
+        private int year;
+        //product list<-----
+
+
+        public InvoiceIn(Supplier sender, WarehouseWorker receiver)
+        {
+            this.delieveredBy = sender;
+            this.receivedBy = receiver;
+        }
+
+        public string getDate()
+        {
+            return String.Join(day.ToString(), "-", month.ToString(), "-", year.ToString());
+        }
+    }
+}
