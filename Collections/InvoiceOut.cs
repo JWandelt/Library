45356f159e45d1101830bb0b21f2e999970e1eb2
Jakub Wandelt 234772 <234772@edu.p.lodz.pl>
2022-04-02 16:05:19 +0200

    InvoiceOut2

--- Collections/InvoiceOut.cs
+++ Collections/InvoiceOut.cs
@@ -8,5 +8,34 @@ namespace DataLayer
 {
     internal class InvoiceOut
     {
+        IUser sentBy;
+        IUser receivedBy;
+        private string invoiceNumber;
+        private int day;
+        private int month;
+        private int year;
+        private float price;
+        //product list to be added to constructor
+        InvoiceOut(WarehouseWorker worker, Client receiver, float price, string invoiceNumber, int day, int month, int year)
+        {
+            this.sentBy = worker;
+            this.receivedBy = receiver;
+            Price = price;
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public string InvoiceNumber { get { return invoiceNumber; } set { invoiceNumber = value; } }
+        public int Day { get { return day; } set { day = value; } }
+        public int Month { get { return month; } set { month = value; } }
+        public int Year { get { return year; } set { year = value; } }
+        public float Price { get { return price; } set { price = value; } }
+
+        public string getDate()
+        {
+            return String.Join(day.ToString(), "-", month.ToString(), "-", year.ToString());
+        }
+
     }
 }
