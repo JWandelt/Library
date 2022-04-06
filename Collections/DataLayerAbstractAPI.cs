33a1d96ea860094f9ba89ff0b19b80586902b44d
Jakub Wandelt 234772 <234772@edu.p.lodz.pl>
2022-04-06 17:59:30 +0200

    getters, setters and removers

--- Collections/DataLayerAbstractAPI.cs
+++ Collections/DataLayerAbstractAPI.cs
@@ -8,6 +8,25 @@ namespace DataLayer
 {
     public abstract class DataLayerAbstractAPI
     {
+        public abstract IUser getClient(int index);
+        public abstract void addClient(IUser client);
+        public abstract void removeClient(int index);
+        public abstract IUser getWorker(int index);
+        public abstract void addWorker(IUser worker);
+        public abstract void removeWorker(int index);
+        public abstract IUser getSupplier(int index);
+        public abstract void addSupplier(IUser supplier);
+        public abstract void removeSupplier(int index);
+        public abstract InvoiceIn getInvoiceIn(int index);
+        public abstract void addInvoiceIn(InvoiceIn invoice);
+        public abstract void removeInvoiceIn(int index);
+        public abstract InvoiceOut getInvoiceOut(int index);
+        public abstract void addInvoiceOut(InvoiceOut invoice);
+        public abstract void removeInvoiceOut(int index);
+        public abstract Product getProduct(int index);
+        public abstract void addProduct(Product product);
+        public abstract void removeProduct(int index);
+
         public abstract void Connect();
 
         public static DataLayerAbstractAPI CreateLinq2SQL(List<IUser> clients, List<IUser> workers, List<IUser> suppliers, List<InvoiceIn> invoiceIns, List<InvoiceOut> invoiceOuts, List<Product> products)
@@ -38,11 +57,101 @@ namespace DataLayer
                 this.invoiceOuts = invoiceOuts;
                 this.products = products;
             }
+
+            public override void addClient(IUser client)
+            {
+                clients.Add(client);
+            }
+
+            public override void addInvoiceIn(InvoiceIn invoice)
+            {
+                invoiceIns.Add(invoice);
+            }
+
+            public override void addInvoiceOut(InvoiceOut invoice)
+            {
+                invoiceOuts.Add(invoice);
+            }
+
+            public override void addProduct(Product product)
+            {
+                products.Add(product);
+            }
+
+            public override void addSupplier(IUser supplier)
+            {
+                suppliers.Add(supplier);
+            }
+
+            public override void addWorker(IUser worker)
+            {
+                workers.Add(worker);
+            }
+
             public override void Connect()
             {
                 throw new System.NotImplementedException();
             }
 
+            public override IUser getClient(int index)
+            {
+                return clients[index];
+            }
+
+            public override InvoiceIn getInvoiceIn(int index)
+            {
+                return invoiceIns[index];
+            }
+
+            public override InvoiceOut getInvoiceOut(int index)
+            {
+                return invoiceOuts[index];
+            }
+
+            public override Product getProduct(int index)
+            {
+                return products[index];
+            }
+
+            public override IUser getSupplier(int index)
+            {
+                return suppliers[index];
+            }
+
+            public override IUser getWorker(int index)
+            {
+                return workers[index];
+            }
+
+            public override void removeClient(int index)
+            {
+                clients.RemoveAt(index);
+            }
+
+            public override void removeInvoiceIn(int index)
+            {
+                invoiceIns.RemoveAt(index);
+            }
+
+            public override void removeInvoiceOut(int index)
+            {
+                invoiceOuts.RemoveAt(index);
+            }
+
+            public override void removeProduct(int index)
+            {
+                products.RemoveAt(index);
+            }
+
+            public override void removeSupplier(int index)
+            {
+                suppliers.RemoveAt(index);
+            }
+
+            public override void removeWorker(int index)
+            {
+                workers.RemoveAt(index);
+            }
         }
     }
 }
