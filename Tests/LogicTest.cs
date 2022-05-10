2042dd82dc9b6198d1caff2f495da79f80409166
234772 <234772@edu.p.lodz.pl>
2022-05-10 18:04:30 +0200

    Logic tests using test data only

--- Tests/LogicTest.cs
+++ Tests/LogicTest.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using LogicLayer;
 using DataLayer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tests.TestData;
 
 namespace Tests
 {
@@ -16,20 +17,16 @@ namespace Tests
         public void Startup()
         {
             TestDataGenerator data = new TestDataGenerator();
-            //dataLayer = DataLayerAbstractAPI.CreateLinq2SQL(data);
             logicLayer = LogicLayerAbstractAPI.CreateLayer(data);
         }
-        //DataLayerAbstractAPI dataLayer;
-
-        //private static TestDataGenerator dataGenerator = new TestDataGenerator();
         LogicLayerAbstractAPI logicLayer;
 
         [TestMethod]
         public void sellProduct()
         {
-            Catalog ca1 = new Catalog(1, "Lemon balm", "Calming herb");
+            ICatalog ca1 = new TCatalog(1, "Lemon balm", "Calming herb");
             int initialQuantity = logicLayer.getData().getProduct(0).Quantity;
-            Product p = new Product(1, 1, 10.0f, ca1);
+            IState p = new TProduct(1, 1, 10.0f, ca1);
             List<IState> productList = new List<IState>();
             productList.Add(p);
             logicLayer.sellProduct(logicLayer.getData().getClient(0), logicLayer.getData().getWorker(0), productList, "OUT/00041/2022", 30, 02, 2022);
@@ -44,9 +41,9 @@ namespace Tests
         [TestMethod]
         public void buyProduct()
         {
-            Catalog ca1 = new Catalog(1, "Lemon balm", "Calming herb");
+            ICatalog ca1 = new TCatalog(1, "Lemon balm", "Calming herb");
             int initialQuantity = logicLayer.getData().getProduct(0).Quantity;
-            Product p = new Product(1, 1, 10.0f, ca1);
+            IState p = new TProduct(1, 1, 10.0f, ca1);
             List<IState> productList = new List<IState>();
             productList.Add(p);
             logicLayer.buyProduct(logicLayer.getData().getSupplier(0), logicLayer.getData().getWorker(0), productList, "IN/00021/2022", 29, 02, 2022);
@@ -54,7 +51,6 @@ namespace Tests
             Assert.AreEqual(logicLayer.getData().getSupplier(0), logicLayer.getData().getInvoiceIn(1).Sender);
             Assert.AreEqual("IN/00021/2022", logicLayer.getData().getInvoiceIn(1).InvoiceNumber);
             Assert.AreEqual(10.0f, logicLayer.getData().getInvoiceIn(1).Price);
-            //Assert.AreEqual("29-2-2022", logicLayer.getData().getInvoiceIn(1).getDate());
         }
     }
 }
