a9e262d9e7dca3bd15de23e5883f052d04cc63d2
234772 <234772@edu.p.lodz.pl>
2022-05-15 16:23:34 +0200

    DataContext and DataAbstractAPI scaffolding

---
+++ Task2/DataLayer/DataAbstractAPI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public abstract class DataAbstractAPI
+    {
+        public abstract void AddLentBook();
+        public abstract void AddBookOnHand();
+        public abstract void RemoveLentBook();
+        public abstract void RemoveBookOnHand();
+        public static LibraryDatabaseDataContext CreateLINQ2SQL()
+        {
+            return DataContext.getDatabase();
+        }
+        public class ProductionDataAPI : DataAbstractAPI
+        {
+            public override void AddBookOnHand()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void AddLentBook()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void RemoveBookOnHand()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void RemoveLentBook()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
