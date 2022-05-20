6db6e2d5e4d8b48225796cc2a84f49d46f1de9dd
234772 <234772@edu.p.lodz.pl>
2022-05-20 17:23:21 +0200

    Data api abstract definition

--- Task2/DataLayer/DataAbstractAPI.cs
+++ Task2/DataLayer/DataAbstractAPI.cs
@@ -8,32 +8,74 @@ namespace DataLayer
 {
     public abstract class DataAbstractAPI
     {
-        public abstract void AddLentBook();
-        public abstract void AddBookOnHand();
-        public abstract void RemoveLentBook();
-        public abstract void RemoveBookOnHand();
-        //public static LibraryDatabaseDataContext CreateLINQ2SQL()
-        //{
-        //    return DataContext.getDatabase();
-        //}
+        //CRUD operations abstract declaration
+        public abstract void addBook(string title, string first_name, string last_name, string description, bool lent);
+        public abstract void removeBook(int id);
+        public abstract void editBook(int id, string title, string first_name, string last_name, string description, string lent);
+        public abstract void addReader(string first_name, string last_name);
+        public abstract void removeReader(int id);
+        public abstract void editReader(int id, string first_name, string last_name);
+        public abstract void addLendList(int bookID, int readerID);
+        public abstract void removeLendList(int bookID);
+        public abstract void editLendList(int id, string first_name, string last_);
+
+        public static LINQtoSQLDataContext CreateLINQ2SQL()
+        {
+            return DataContext.connectDatabase();
+        }
         public class ProductionDataAPI : DataAbstractAPI
         {
-            public override void AddBookOnHand()
+            private LINQtoSQLDataContext db;
+            ProductionDataAPI(LINQtoSQLDataContext db)
+            {
+                this.db = db;
+            }
+
+            public override void addBook(string title, string first_name, string last_name, string description, bool lent)
+            {
+                List<Ibook> books = new List<Ibook>();
+                book b1 = new book();
+                books.Add((Ibook)b1);
+                //db.books.InsertOnSubmit(b1));
+                db.SubmitChanges();
+            }
+
+            public override void addLendList(int bookID, int readerID)
             {
                 throw new NotImplementedException();
             }
 
-            public override void AddLentBook()
+            public override void addReader(string first_name, string last_name)
             {
                 throw new NotImplementedException();
             }
 
-            public override void RemoveBookOnHand()
+            public override void editBook(int id, string title, string first_name, string last_name, string description, string lent)
             {
                 throw new NotImplementedException();
             }
 
-            public override void RemoveLentBook()
+            public override void editLendList(int id, string first_name, string last_)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void editReader(int id, string first_name, string last_name)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void removeBook(int id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void removeLendList(int bookID)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void removeReader(int id)
             {
                 throw new NotImplementedException();
             }
