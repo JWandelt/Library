448f40036f219be64816227a8868fa73291df105
JWandelt <234772@edu.p.lodz.pl>
2023-02-20 21:36:52 +0100

    Removed unnecessary files

--- Task1/Collections/ICatalog.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataLayer
-{
-    public interface ICatalog
-    {
-        int ItemID { get; set; }
-        string ItemName { get; set; }
-        string ItemShortDescription { get; set; }
-    }
-}
